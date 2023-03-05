af5eaf307678d8742675244d3d2022deadc9f0fe
mikhailkotor <milan3020@gmail.com>
2023-03-05 15:06:36 +0200

    init commit

---
+++ src/test/java/com/kotor/elections/ElectionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kotor.elections;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElectionsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
